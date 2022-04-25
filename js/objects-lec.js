6eddaac9c32473998cf63ccf980d728199faf205
David Stephens <stephens.a.david@gmail.com>
2022-04-25 10:25:38 -0500

    completd the lecture on objects!

--- js/objects-lec.js
+++ js/objects-lec.js
@@ -1,6 +1,6 @@
 "use strict";
 
-(function() {
+(function () {
 
     // We can create an object with properties like so!
     let kamea = {
@@ -12,7 +12,7 @@
     };
 
     // We can also define property values to an object these two ways!
-    kamea.age = 6;
+    kamea.age = 8;
     kamea["weight_in_lbs"] = 2400;
 
     let babyBeluga = {
@@ -21,13 +21,13 @@
         colors: ["White"],
         species: "Beluga",
         age: 2,
-        date_of_birth: new Date("2/6/2018")
+        date_of_birth: new Date("2/6/2020")
     };
 
     // We can access the properties but using the following syntax.
-    // console.log(kamea.age);
-    //
-    // console.log(babyBeluga.name + " in " + babyBeluga.location + ", he swim so wild, and he swim so free...");
+    console.log(kamea.age);
+
+    console.log(babyBeluga.name + " in " + babyBeluga.location + ", he swim so wild, and he swim so free...");
 
     // Well, whales are neat and all, but what would be a more practical application for objects?
 
@@ -35,11 +35,12 @@
         {
             day: "Monday",
             summary: "Partly cloudy.",
+            bearableWeather,
             highsLows: {
-                highActual: 92,
-                highFeels: 96,
+                highActual: 78,
+                highFeels: 74,
                 lowActual: 72,
-                lowFeels: 74
+                lowFeels: 72
             },
             icon: "../img/part_cloud.jpg", // This will make more sense when we start talking about manipulating the page using JS.
             location: {
@@ -52,6 +53,7 @@
         }, {
             day: "Tuesday",
             summary: "Sunny.",
+            bearableWeather,
             highsLows: {
                 highActual: 99,
                 highFeels: 99,
@@ -69,11 +71,12 @@
         }, {
             day: "Wednesday",
             summary: "Rainy.",
+            bearableWeather,
             highsLows: {
-                highActual: 88,
-                highFeels: 92,
-                lowActual: 68,
-                lowFeels: 66
+                highActual: 68,
+                highFeels: 66,
+                lowActual: 63,
+                lowFeels: 62
             },
             icon: "../img/rain.jpg",
             location: {
@@ -86,11 +89,22 @@
         }
     ];
 
+    function bearableWeather() {
+        if (this.highsLows.highFeels <= 74 && this.highsLows.lowFeels >= 62 && this.summary !== "Rainy.") {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    console.log(bearableWeather());
+
     // Above is an array of weather forecasts, here we can see nested objects within the different objects in the array.
 
-    // for(let prediction of forecast) {
-    //     console.log(prediction.day);
-    // }
+    for (let prediction of forecast) {
+        console.log(prediction.day + ": " + prediction.highsLows.highFeels + " | " + prediction.highsLows.lowFeels);
+        console.log("Bearable weather: " + prediction.bearableWeather());
+    }
 
     // While we store the different forecasts in an array to hold the information for the three different days, each object has different information in order to store the information for a daily weather forecast.
 
@@ -100,31 +114,28 @@
         username: "JimLikesEggs",
         password: "this-is-am-good-password",
         avatar_img: 'path/to/file.jpg',
-        d_o_b: new Date("2/17/1967")
+        d_o_b: new Date("Nov 3, 1967")
     };
 
     // with this user object, let's add a method:
 
-    user.calcAge = function() {
+    user.calcAge = function () {
         let today = new Date();
-        if (this.d_o_b.getMonth() <= today.getMonth()){
-            if (this.d_o_b.getDate() <= today.getDate()) {
-                this.age = today.getFullYear() - this.d_o_b.getFullYear();
-            } else {
-                this.age = today.getFullYear() - this.d_o_b.getFullYear() - 1;
-            }
+        if (this.d_o_b.getMonth() <= today.getMonth() && this.d_o_b.getDate() <= today.getDate()) {
+            this.age = today.getFullYear() - this.d_o_b.getFullYear();
         } else {
             this.age = today.getFullYear() - this.d_o_b.getFullYear() - 1;
         }
         return this.age;
     };
 
-    // console.log(user.calcAge());
+    console.log(user.calcAge());
 
     // Notice with the method we used the "this" keyword. This allows us to have an object to reference itself when creating a method. It will not work when trying to define a property.
 
-    // user.name = this.username; // This will give an error.
+    user.name = this.username; // This will give an error.
+    user.name = user.username;
     //
-    // console.log(user.name);
+    console.log(user.name);
 
 })();
