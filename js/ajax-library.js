7782ed5bf77e9c2f36f70025efe3f49f62fed83a
David Stephens <stephens.a.david@gmail.com>
2022-05-25 09:27:39 -0500

    complete the lecture for AJAX in a way that isn't brain breaking

---
+++ js/ajax-library.js
@@ -0,0 +1,8 @@
+$.get("/data/library.json").done(function (books) {
+    $("#books").html("");
+    books.forEach(function(book) {
+        var h1 = "<h1>" + book.title + "</h1>";
+        var p = "<p>By: " + book.author + "</p>"
+        $("#books").append(h1 + p);
+    });
+});
